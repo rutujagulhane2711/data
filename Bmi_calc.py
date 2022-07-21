85d4b913bc632ae957c143b8a6440ace6eff3019
rutujagulhane2711 <109725847+rutujagulhane2711@users.noreply.github.com>
2022-07-21 13:53:15 +0530

    Update Bmi_calc.py

--- Bmi_calc.py
+++ Bmi_calc.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import json
 
-lst = [{"Gender": "Male", "HeightCm": 171, "WeightKg": 200},
+lst = [{"Gender": "Male", "HeightCm": 171, "WeightKg": 96},
            {"Gender": "Male", "HeightCm": 161, "WeightKg": 85},
            {"Gender": "Male", "HeightCm": 180, "WeightKg": 77},
            {"Gender": "Female", "HeightCm": 166, "WeightKg": 62},
-           {"Gender": "male", "HeightCm": 167, "WeightKg": 82},
+           {"Gender": "Female", "HeightCm": 150, "WeightKg": 70},
            {"Gender": "Female", "HeightCm": 167, "WeightKg": 82}]
 final_lst = []
 
